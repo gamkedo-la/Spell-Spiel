20ff9c298d07774b9ecb4bcd5482561ee1284803
sahilharidas <sahilharidas@gmail.com>
2017-07-17 17:14:02 +0200

    reafactored js files and images

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -17,18 +17,18 @@ function countLoadedImagesAndLaunchIfReady(){
 
 function beginLoadingImage(imgVar, fileName){
 	imgVar.onload = countLoadedImagesAndLaunchIfReady();
-	imgVar.src = fileName;
+	imgVar.src = "images/" + fileName;
 }
 
 
 function loadImages(){
 
     var imageList = [
-        { varName: standingPic, theFile: "Graphics/standing.png" },
-        { varName: fillerPic, theFile: "Graphics/pommier3.png" },
-        { varName: battlePic, theFile: "Graphics/bgResized.png" },
-        { varName: overworldPic, theFile: "Graphics/study.png" },
-        { varName: batPic, theFile: "Graphics/bat.png" }
+        { varName: standingPic, theFile: "standing.png" },
+        { varName: fillerPic, theFile: "pommier3.png" },
+        { varName: battlePic, theFile: "bgResized.png" },
+        { varName: overworldPic, theFile: "study.png" },
+        { varName: batPic, theFile: "bat.png" }
     ]
 
     picsToLoad = imageList.length; //This method avoids a race condition
